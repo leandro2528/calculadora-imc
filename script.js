793778bd91258b5ef602ed15d6479f72c47fd648
Leandro <leandrodev83@gmail.com>
2023-02-15 01:31:11 -0300

    Adicionando uma funçaõ par fazer os calculos de forma mais dinâmica  em tempo real

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function start() {
     var btnCalculator = document.querySelector('#btnCalculator');
     btnCalculator.addEventListener('click', handleButtonCalculator);
+
+    var inputWeight = document.querySelector('#input-weight');
+    var inputHeight = document.querySelector('#input-height');
+
+    inputWeight.addEventListener('input', handleButtonCalculator);
+    inputHeight.addEventListener('input', handleButtonCalculator);
+
+    handleButtonCalculator();
 }
 
 function calculatorImc(weight, height) {
@@ -10,11 +18,12 @@ function calculatorImc(weight, height) {
 function handleButtonCalculator() {
     var inputWeight = document.querySelector('#input-weight').value;
     var inputHeight = document.querySelector('#input-height').value;
+
     var imcResult = document.querySelector('#imcResult');
 
     var imc = calculatorImc(inputWeight, inputHeight);
 
-    imcResult.textContent = imc;
+    imcResult.textContent = imc.toFixed(2).replace('.', ',');
 
 }
 
